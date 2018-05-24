50a38a06cfa55e2ca87077df38311afccc02e9dc
赵岳峰 <13317312768@qq.com>
2018-05-24 18:20:12 +0800

    添加封装实现

--- src/main/java/gateway/api/Return.java
+++ src/main/java/gateway/api/Return.java
@@ -9,12 +9,43 @@ package gateway.api;
 public class Return<T> extends Result {
 
 	private static final long serialVersionUID = -6422710904853351605L;
-	
+		
 	public T data;
 	
 	@Override
 	public String toString() {
 		return JsonUtils.toJson(this);
+	}
+	
+	public static <T> Return<T> wrap(T obj) {
+		Return<T> wval = new Return<T>();
+		wval.data = obj;
+		return wval;
+	}
+	
+	public static <T> Return<T> success() {
+		Return<T> wval = new Return<T>();
+		return wval;
+	}
+	
+	public static <T> Return<T> success(String msg) {
+		Return<T> wval = new Return<T>();
+		wval.message = msg;
+		return wval;
+	}
+	
+	public static <T> Return<T> error(int code) {
+		Return<T> wval = new Return<T>();
+		wval.code = code;
+		wval.message = "调用出现错误！";
+		return wval;
+	}
+	
+	public static <T> Return<T> error(int code, String msg) {
+		Return<T> wval = new Return<T>();
+		wval.code = code;
+		wval.message = msg;
+		return wval;
 	}
 	
 }
